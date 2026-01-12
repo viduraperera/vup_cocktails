0951f40e662e5be1da79032e163e0baf1d7623af
viduraperera <laumayangaperera@gmail.com>
2026-01-12 14:33:34 +0530

    just for testing

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,9 @@ gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const App = () => {
     return (
-        <div className="flex-center h-screen">
+        <div className="flex-center h-screen" style={{display: "flex", flexDirection: "column"}}>
             <h1 className='text-3xl text-indigo-300'>Hello, GSAP!</h1>
+            <h2 className='text-2xl text-indigo-200'>Test, GSAP!</h2>
         </div>
     );
 };
